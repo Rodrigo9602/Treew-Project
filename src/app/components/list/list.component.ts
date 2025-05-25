8aaac58a82813e45dfd98714a769e20ef1106748
Rodrigo9602 <119463817+Rodrigo9602@users.noreply.github.com>
2025-05-25 07:04:04 -0400

    feat: list component
    Added List Component which will be used for Trello List visualization

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { TrelloCard, TrelloList } from '../../services/authorization.service';
+import { TrelloAuthService } from '../../services/authorization.service';
+
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-list',
+  imports: [CardComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent implements OnInit {
+  @Input() list: TrelloList | undefined;
+  public cardsList: TrelloCard[] = [];
+
+  constructor(private trelloService: TrelloAuthService) {}
+
+  ngOnInit(): void {
+    // Obtener listado de tarjetas para la lista actual
+    if (this.list) {
+      this.trelloService.getListCards(this.list.id).subscribe((cards) => {
+        this.cardsList = cards;        
+      });
+    }
+  }
+}
