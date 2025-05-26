f68176a816be856f2b55369ec403da710bb020bc
Rodrigo9602 <119463817+Rodrigo9602@users.noreply.github.com>
2025-05-26 04:05:51 -0400

    bug fix: added toast container on root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ToastContainerComponent } from './components/toast';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
